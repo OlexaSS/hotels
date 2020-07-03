c46b9f394e7b9dbcec1b9388e93dd0682a468c3e
Александр <54662251+OlexaSS@users.noreply.github.com>
2020-07-03 09:49:25 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="img/next.svg" alt="next arrow"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="img/prev.svg" alt="prev arrow"></button>',
+        fade: true,
+        responsive: [
+            {    
+                breakpoint: 441,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+
+    new WOW().init();
+});
+
+
